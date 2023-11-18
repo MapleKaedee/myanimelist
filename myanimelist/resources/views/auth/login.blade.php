f8cac3e21de65412650ecf056efc3c7f2faacd06
Maple <maplemepuruu@gmail.com>
2023-11-19 04:29:45 +0700

    user auth and routing auth

--- myanimelist/resources/views/auth/login.blade.php
+++ myanimelist/resources/views/auth/login.blade.php
@@ -39,6 +39,12 @@
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
+            @if (Route::has('register'))
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    href="{{ route('register') }}">
+                    {{ __('Dont Have An Account?') }}
+                </a>
+            @endif
 
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
