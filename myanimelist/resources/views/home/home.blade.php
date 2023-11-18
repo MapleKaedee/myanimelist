22d52883611302629c07e61f9d0c615688e783c8
Nafunnn <aanalim31@gmail.com>
2023-11-18 22:29:11 +0700

    add design hero and new added

--- myanimelist/resources/views/home/home.blade.php
+++ myanimelist/resources/views/home/home.blade.php
@@ -61,8 +61,75 @@
         </div>
     </div>
     {{-- NAVBAR END --}}
+
+    {{-- HERO START --}}
     <div class="m-12">
-            halo
+        <h1 class="mb-4 text-2xl font-bold">News</h1>
+        <div class="rounded-md hero bg-blue-50">
+            <div class="hero-content flex-col lg:flex-row">
+                <img src="https://i.pinimg.com/564x/e9/a2/3b/e9a23b37bc304e72db21ad2680b87ee5.jpg" class="h-64 rounded-lg shadow-2xl" />
+                <div>
+                    <div class="flex gap-2">
+                        <h1 class="badge badge-outline">Action</h1>
+                        <h1 class="badge badge-outline">Drama</h1>
+                    </div>
+                    <h1 class="text-5xl font-bold">Sword Art Online : Progressive</h1>
+                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <a class="btn bg-blue-600 text-white">Watch Now!</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- HERO END --}}
+    <div class="m-12">
+        <div class="flex justify-between items-center mb-4">
+            <h1 class=" text-2xl font-bold align-middle">New Added</h1>
+            <a class="btn btn-sm btn-outline btn-info" href="">Add New</a>
+        </div>
+        <div class="grid grid-cols-5 gap-6">
+            <div class="card bg-base-100 shadow-xl">
+                <figure><img src="https://i.pinimg.com/564x/b6/4e/4e/b64e4e6cf83da7959b22038ef7097105.jpg" alt="Shoes" /></figure>
+                <div class="m-4">
+                    <h2 class="card-title">Bofuri</h2>
+                    <p>Anime Kesukaan Wahit Ini</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Watch</button>
+                    </div>
+                </div>
+            </div>
+            <div class="card bg-base-100 shadow-xl">
+                <figure><img src="https://i.pinimg.com/564x/b6/4e/4e/b64e4e6cf83da7959b22038ef7097105.jpg" alt="Shoes" /></figure>
+                <div class="m-4">
+                    <h2 class="card-title">Bofuri</h2>
+                    <p>Anime Kesukaan Wahit Ini</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Watch</button>
+                    </div>
+                </div>
+            </div>
+            <div class="card bg-base-100 shadow-xl">
+                <figure><img src="https://i.pinimg.com/564x/b6/4e/4e/b64e4e6cf83da7959b22038ef7097105.jpg" alt="Shoes" /></figure>
+                <div class="m-4">
+                    <h2 class="card-title">Bofuri</h2>
+                    <p>Anime Kesukaan Wahit Ini</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Watch</button>
+                    </div>
+                </div>
+            </div>
+            <div class="card bg-base-100 shadow-xl">
+                <figure><img src="https://i.pinimg.com/564x/b6/4e/4e/b64e4e6cf83da7959b22038ef7097105.jpg" alt="Shoes" /></figure>
+                <div class="m-4">
+                    <h1 class="text-xs font-semibold">Action</h1>
+                    <h2 class="card-title">Bofuri</h2>
+                    <p>Anime Kesukaan Wahit Ini</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Watch</button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
 </body>
 </html>
