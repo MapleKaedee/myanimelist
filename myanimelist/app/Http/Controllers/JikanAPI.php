0a1768c1a0d4826674ad40749cc4d3f582207d4a
Maple <maplemepuruu@gmail.com>
2023-11-22 20:53:22 +0700

    pagination

--- myanimelist/app/Http/Controllers/JikanAPI.php
+++ myanimelist/app/Http/Controllers/JikanAPI.php
@@ -2,38 +2,95 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Log;
 
 class JikanAPI extends Controller
 {
-    public function anime()
+    private function FetchAnime()
     {
         $client = new Client();
-        $response = $client->request('GET', getenv('URL_BASE') . '/seasons/now');
-        $data = json_decode($response->getBody()->getContents(), true);
+        $allAnime = [];
+        $animeCount = 0;
 
-        if (!empty($data) && isset($data['data'])) {
-            $animeList = $data['data'];
-            $animes = [];
+        $currentPage = 1; // Halaman awal
+        $lastPage = null;
 
-            // Loop through each anime and retrieve the data
-            foreach ($animeList as $anime) {
-                $animes[] = [
-                    'title' => $anime['title'],
-                    'synopsis' => $anime['synopsis'],
-                    'images' => $anime['images'],
-                    'type' => $anime['type'],
-                    'genres' => $anime['genres'],
-                    'producer' => $anime['producers'][0]['name'],
-                    'episodes' => $anime['episodes'],
-                    'season' => $anime['season'],
-                    'year' => $anime['year'],
-                ];
+        do {
+            try {
+                $response = $client->request('GET', 'https://api.jikan.moe/v4/top/anime?page=' . $currentPage);
+
+                $statusCode = $response->getStatusCode();
+
+                if ($statusCode == 200) {
+                    $data = json_decode($response->getBody(), true);
+
+                    // Hitung berapa banyak anime yang sudah diambil
+                    $currentAnimeCount = count($data['data']);
+                    $remainingSpace = 200 - $animeCount;
+
+                    // Jika anime yang diterima lebih dari ruang yang tersisa, ambil sebagian data yang cukup
+                    if ($currentAnimeCount > $remainingSpace) {
+                        $data['data'] = array_slice($data['data'], 0, $remainingSpace);
+                    }
+
+                    // Masukkan data dari halaman saat ini ke dalam array semua anime
+                    $allAnime = array_merge($allAnime, $data['data']);
+
+                    // Update informasi paginasi
+                    $currentPage = $data['pagination']['current_page'];
+                    $lastPage = $data['pagination']['last_visible_page'];
+
+                    // Tambahkan jumlah anime yang telah diambil
+                    $animeCount += $currentAnimeCount;
+
+                    // Tambahkan waktu tunggu sebelum melakukan permintaan berikutnya
+                    usleep(200000); // Delay 200ms (2/10 detik) sebelum melakukan permintaan lagi
+
+                    // Periksa apakah ada halaman selanjutnya
+                    if ($animeCount < 200 && $data['pagination']['has_next_page']) {
+                        $currentPage++; // Pindah ke halaman selanjutnya
+                    }
+                } else {
+                    // Handle jika respons tidak berhasil
+                    return response('Failed to fetch data from API', $statusCode);
+                }
+            } catch (RequestException $e) {
+                Log::error('RequestException: ' . $e->getMessage());
+                // Handle jika terjadi exception
+                return response('Failed to fetch data from API: ' . $e->getMessage(), $e->getCode());
+            } catch (Exception $e) {
+                Log::error('Exception: ' . $e->getMessage());
+                // Handle jika terjadi exception lainnya
+                return response('Failed to fetch data from API: ' . $e->getMessage());
             }
+        } while ($animeCount < 200 && $currentPage <= $lastPage); // Lakukan permintaan berulang sampai batas 100 anime tercapai
+        return $allAnime;
+    }
 
-            return $animes;
-        }
-        return [];
+    public function Anime()
+    {
+        // Mendapatkan data anime
+        $allAnime = $this->FetchAnime();
+
+        // Melakukan pagination
+        $perPage = 10; // Jumlah anime per halaman
+        $currentPage = request()->input('page', 1); // Mendapatkan halaman saat ini dari URL parameter
+
+        $pagedData = array_slice($allAnime, ($currentPage - 1) * $perPage, $perPage);
+
+        $paginatedData = new LengthAwarePaginator(
+            $pagedData,
+            count($allAnime),
+            $perPage,
+            $currentPage,
+            ['path' => request()->url(), 'query' => request()->query()]
+        );
+        return $paginatedData;
+
     }
     public function getAnime()
     {
@@ -41,17 +98,17 @@ class JikanAPI extends Controller
         $response = $client->request('GET', 'https://api.jikan.moe/v4/anime/41514');
         $data = json_decode($response->getBody(), true);
 
-        $specificAninme = $data['data'];
-        return $specificAninme;
+        $specificAnime = $data['data'];
+        return $specificAnime;
     }
 
     public function showView()
     {
-        $animes = $this->anime();
-        $specificAninme = $this->getAnime();
+        $paginatedData = $this->Anime();
+        $specificAnime = $this->getAnime();
 
         return view('home.home')
-            ->with('animes', $animes)
-            ->with('specificAninme', $specificAninme);
+            ->with('paginatedData', $paginatedData)
+            ->with('specificAnime', $specificAnime);
     }
 }
--- myanimelist/resources/views/home/home.blade.php
+++ myanimelist/resources/views/home/home.blade.php
@@ -4,16 +4,18 @@
         <h1 class="mb-4 text-2xl font-bold dark:text-white">Trending Now</h1>
         <div class="rounded-md hero bg-slate-50 dark:bg-slate-600 shadow-md dark:text-white">
             <div class="hero-content flex-col lg:flex-row">
-                <img src="{{ $specificAninme['images']['jpg']['image_url'] }}" class="h-64 rounded-lg shadow-2xl" />
+                <img src="{{ $specificAnime['images']['jpg']['image_url'] }}" class="h-64 rounded-lg shadow-2xl" />
                 <div>
                     <div class="flex gap-2 my-2">
-                        <h1 class="badge badge-outline">{{ $specificAninme['genres'][0]['name'] }}</h1>
+                        @foreach ($specificAnime['genres'] as $specAnime)
+                            <h1 class="badge badge-outline">{{ $specAnime['name'] }}</h1>
+                        @endforeach
                     </div>
-                    <h1 class="text-5xl font-bold">{{ $specificAninme['title'] }}</h1>
-                    <p class="py-6">{{ $specificAninme['synopsis'] }}</p>
+                    <h1 class="text-5xl font-bold">{{ $specificAnime['title'] }}</h1>
+                    <p class="py-6 textTitleContainer">{{ $specificAnime['synopsis'] }}</p>
                     <a
-                        class="btn bg-blue-600 dark:bg-slate-700 hover:dark:bg-slate-300 hover:dark:text-slate-700 text-white">Watch
-                        Now!</a>
+                        class="btn bg-blue-600 dark:bg-slate-700 hover:dark:bg-slate-300 hover:dark:text-slate-700 text-white">See
+                        Details</a>
                 </div>
             </div>
         </div>
@@ -21,27 +23,18 @@
     {{-- HERO END --}}
     <div class="m-12">
         <div class="flex justify-between items-center mb-4 dark:text-white">
-            <h1 class=" text-2xl font-bold align-middle">New Added</h1>
+            <h1 class="text-2xl font-bold align-middle">New Added</h1>
             <a class="btn btn-sm btn-outline btn-info dark:border-white dark:text-white" href="">Add New</a>
         </div>
         <div class="grid md:grid-cols-5 grid-cols-2 sm:grid-cols-3 gap-6">
-            @foreach ($animes as $anime)
+            @foreach ($paginatedData as $anime)
                 <div class="card bg-base-100 dark:bg-slate-700 dark:text-white shadow-xl">
                     <figure>
-                        @foreach ($anime['images'] as $imageType => $image)
-                            @if ($imageType === 'jpg')
-                                <img class="aspect-auto" src="{{ $image['image_url'] }}"
-                                    alt="{{ $image['large_image_url'] }}" />
-                            @endif
-                        @endforeach
-
+                        <img class="aspect-auto" src="{{ $anime['images']['jpg']['image_url'] }}" alt="" />
                     </figure>
-
                     <div class="m-4">
                         <div class="mb-2">
-                            @foreach ($anime['genres'] as $genre)
-                                <h1 class="badge text-xs font-semibold">{{ $genre['name'] }}</h1>
-                            @endforeach
+                            <h1 class="badge text-xs font-semibold">{{ $anime['genres'][0]['name'] }}</h1>
                         </div>
                         <h2 class="card-title">{{ $anime['title'] }}</h2>
                         <p class="textContainer dark:text-slate-300">{{ $anime['synopsis'] }}</p>
@@ -51,7 +44,33 @@
                     </div>
                 </div>
             @endforeach
+        </div>
+        <div class="join">
+            @php
+                $currentPage = $paginatedData->currentPage();
+                $lastPage = $paginatedData->lastPage();
+            @endphp
 
+            <button class="join-item btn" onclick="window.location='{{ $paginatedData->url(1) }}'">1</button>
+            <button class="join-item btn" onclick="window.location='{{ $paginatedData->url(2) }}'">2</button>
+
+            @if ($currentPage > 3)
+                <button class="join-item btn btn-disabled">...</button>
+            @endif
+
+            @for ($i = max(3, $currentPage - 1); $i <= min($currentPage + 1, $lastPage - 1); $i++)
+                <button class="join-item btn {{ $i == $currentPage ? 'active' : '' }}"
+                    onclick="window.location='{{ $paginatedData->url($i) }}'">{{ $i }}</button>
+            @endfor
+
+            @if ($currentPage < $lastPage - 2)
+                <button class="join-item btn btn-disabled">...</button>
+            @endif
+
+            <button class="join-item btn"
+                onclick="window.location='{{ $paginatedData->url($lastPage - 1) }}'">{{ $lastPage - 1 }}</button>
+            <button class="join-item btn"
+                onclick="window.location='{{ $paginatedData->url($lastPage) }}'">{{ $lastPage }}</button>
         </div>
     </div>
 
