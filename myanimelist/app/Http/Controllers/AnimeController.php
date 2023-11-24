f8cdfa35eb3954e25d35f93a926facaeb3142a10
Nafunnn <aanalim31@gmail.com>
2023-11-24 11:28:57 +0700

    add trending now at search.blade.php, update AnimeController, change layout search box, centering pagination

--- myanimelist/app/Http/Controllers/AnimeController.php
+++ myanimelist/app/Http/Controllers/AnimeController.php
@@ -3,23 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Http;
+use GuzzleHttp\Client;
 
 class AnimeController extends Controller
 {
+    public function Anime(){
+        $client = new Client();
+        $response = $client->request('GET', 'https://api.jikan.moe/v4/anime/41514');
+        $data = json_decode($response->getBody(), true);
+
+        $specificAnime = $data['data'];
+        return $specificAnime;
+    }
+
     public function getAnime(Request $request)
     {
         $animeName = $request->input('anime_name'); // Assuming the input name is 'anime_name'
         $apiUrl = 'https://api.jikan.moe/v4/anime?q=' . urlencode($animeName);
 
         try {
+            $specificAnime = $this->Anime();
             $response = Http::get($apiUrl);
             $animeData = $response->json()['data'];
 
-            return view('search.search', compact('animeData'));
+            return view('search.search', compact('animeData', 'specificAnime'));
         } catch (\Exception $e) {
             // Handle errors, e.g., redirect back with an error message
             return redirect()->back()->with('error', 'Failed to fetch anime data.');
         }
     }
+
+
 }
--- myanimelist/resources/views/search/search.blade.php
+++ myanimelist/resources/views/search/search.blade.php
@@ -1,8 +1,28 @@
 <x-nav-layout>
-    <div class="join">
+    <div class="m-12">
+        <h1 class="mb-4 text-2xl font-bold dark:text-white">Trending Now</h1>
+        <div class="rounded-md hero bg-slate-50 dark:bg-slate-600 shadow-md dark:text-white">
+            <div class="hero-content flex-col lg:flex-row">
+                <img src="{{ $specificAnime['images']['jpg']['image_url'] }}" class="h-64 rounded-lg shadow-2xl" />
+                <div>
+                    <div class="flex gap-2 my-2">
+                        @foreach ($specificAnime['genres'] as $specAnime)
+                            <h1 class="badge badge-outline">{{ $specAnime['name'] }}</h1>
+                        @endforeach
+                    </div>
+                    <h1 class="text-5xl font-bold">{{ $specificAnime['title'] }}</h1>
+                    <p class="py-6 textTitleContainer">{{ $specificAnime['synopsis'] }}</p>
+                    <a
+                        class="btn bg-blue-600 dark:bg-slate-700 hover:dark:bg-slate-300 hover:dark:text-slate-700 text-white">See
+                        Details</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="mx-12 mt-12">
         <form method="GET" action="{{ url('/search') }}">
-            <input class="input input-bordered join-item" type="text" name="anime_name" placeholder="Enter Anime Name" />
-            <button class="btn join-item rounded-r-full" type="submit">Search</button>
+            <input class="input input-bordered w-full max-w-xs dark:bg-slate-400" type="text" name="anime_name" placeholder="Enter Anime Name" />
+            <button class="btn btn-outline dark:text-slate-100 dark:border-slate-100" type="submit">Search</button>
         </form>
     </div>
     <div class="m-12">
@@ -11,7 +31,7 @@
                 @foreach ($animeData as $anime)
                     <div class="card bg-base-100 dark:bg-slate-700 dark:text-white shadow-xl">
                         <figure>
-                            <img class="aspect-auto" src="{{ $anime['images']['jpg']['image_url'] }}" alt="" />
+                            <img class="aspect-square" src="{{ $anime['images']['jpg']['image_url'] }}" alt="" />
                         </figure>
                         <div class="m-4">
                             <div class="mb-2">
